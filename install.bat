afce5f0bb8faee3ceb341916cf713b89e95bad47
Milos Stevanovic <mstevanovic2@gvcgroup.com>
2018-12-09 11:35:14 +0100

    Add ideavimrc to install.bat

--- install.bat
+++ install.bat
@@ -1,3 +1,6 @@
 mklink "%USERPROFILE%\.vsvimrc" %cd%\vsvimrc
 mklink "%USERPROFILE%\.debug_map.vimrc" %cd%\debug_map.vimrc
 mklink "%USERPROFILE%\.debug_unmap.vimrc" %cd%\debug_unmap.vimrc
+mklink "%USERPROFILE%\.ideavimrc" %cd%\ideavimrc
+mklink "%USERPROFILE%\.debug_unmap.ideavimrc" %cd%\debug_unmap.ideavimrc
+mklink "%USERPROFILE%\.debug_map.ideavimrc" %cd%\debug_map.ideavimrc
